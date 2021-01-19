687c53b473748d21c638cc48196a5b59626c4a9c
meunostu <mihdevel@gmail.com>
2021-01-19 15:00:02 +0300

    Added(ft_utils.c) ft_itoa_base

---
+++ srcs/ft_utils.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meunostu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/04 10:58:12 by meunostu          #+#    #+#             */
+/*   Updated: 2021/01/19 14:36:12 by meunostu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf.h"
+
+char		*ft_get_next_argument_char(va_list argptr)
+{
+	char	*arg;
+
+	arg = va_arg(argptr, char *);
+	return (arg);
+}
+
+int			ft_get_next_argument_int(va_list argptr)
+{
+	int		arg;
+
+	arg = va_arg(argptr, int);
+	return (arg);
+}
+
+int		ft_abs(int nb)
+{
+	if (nb < 0)
+		nb = -nb;
+	return (nb);
+}
+
+char	*ft_itoa_base(int value, int base)
+{
+	char	*str;
+	int		size;
+	char	*tab;
+	int		flag;
+	int		tmp;
+	flag = 0;
+	size = 0;
+	tab = "0123456789ABCDEF";
+	if (base < 2 || base > 16)
+		return (0);
+	if (value < 0 && base == 10)
+		flag = 1;
+	tmp = value;
+	while (tmp /= base)
+		size++;
+	size = size + flag + 1;
+	str = (char *)malloc(sizeof(char) * size  + 1);
+	str[size] = '\0';
+	if (flag == 1)
+		str[0] = '-';
+	while (size > flag)
+	{
+		str[size - 1] = tab[ft_abs(value % base)];
+		size--;
+		value /=base;
+	}
+	return (str);
+}
